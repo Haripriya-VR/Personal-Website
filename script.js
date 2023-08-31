d3ddc6da98cdaf3b6e9d422de74410956b4ffb44
Haripriya-VR <hpvr80343@gmail.com>
2023-08-31 16:19:29 +0530

    added menu bar

---
+++ script.js
@@ -0,0 +1,10 @@
+const sideMenu = document.getElementById("side-menu")
+
+function openmenu(){
+    sideMenu.style.right ="0"
+    console.log("hiii");
+}
+function closemenu() {
+    sideMenu.style.right ="-200px"
+    console.log("jii");
+}
